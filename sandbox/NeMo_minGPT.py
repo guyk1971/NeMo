c3b2445a18b2d5ab64d0f0c4c3e03e563e7fe3a0
guyk1971 <guy.koren@gmail.com>
2023-11-06 17:14:19 +0200

    adding sandbox folder for trials

---
+++ sandbox/NeMo_minGPT.py
@@ -0,0 +1,12 @@
+import torch
+import nemo
+from nemo.core import NeuralModule
+from nemo.core import typecheck
+from nemo.core.neural_types import NeuralType
+from nemo.core.neural_types import *
+
+import math
+from typing import List, Set, Dict, Tuple, Optional
+import torch.nn as nn
+from torch.nn import functional as F
+
